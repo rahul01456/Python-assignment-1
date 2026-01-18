8daf227073c49b6124b4813bbf139e73cef28f71
Rahul Kumar <rahul_kumar014@outlook.com>
2026-01-18 18:36:09 +0530

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,12 @@
+a = float(input("Please enter the first number: "))
+b = float(input("Please enter the Second number: "))
+add = a + b
+sub = a - b
+mul = a*b
+div = a/b
+expo = pow(a,b)
+print("Addition od two number is: ",add)
+print("Subtraction of two number is: ", sub)
+print("Multiplication of two number is : ",mul)
+print(" Division of two given number is : ",div)
+print("Exponential of given two number are : ",expo)
